885b8cd667fa5d5e6aaf1474d562ce9f4097962a
Marcos Vinicius <marcosvinicius.lucena27@gmail.com>
2023-06-24 12:13:58 -0300

    Correção no diretório da página de login

--- api/calcs.php
+++ api/calcs.php
@@ -30,7 +30,7 @@
                   target="_blank">Newsletter</a></span>
               <span class="landing-page-text03"><a href="calcs.php">Calculadoras</a></span>
             </div>
-            <a href="../api/pages/subscribe-login.php" class="landing-page-link">
+            <a href="subscribe-login.php" class="landing-page-link">
               <div class="solid-button-container">
                 <button class="solid-button-button button Button" style="cursor: pointer">
                   <span>Login</span>
@@ -71,7 +71,7 @@
                       target="_blank">Newsletter</a></span>
                   <span><a href="calcs.php">Calculadoras</a></span>
                 </div>
-                <a href="../api/pages/subscribe-login.php" class="landing-page-link">
+                <a href="subscribe-login.php" class="landing-page-link">
                   <div class="solid-button-container">
                     <button class="solid-button-button button Button" style="cursor: pointer">
                       <span>Login</span>
