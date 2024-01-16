21db647d554cc58e0a9f881a3cb7551a86c595dd
Marcos Vinicius de lucena <marcosvinicius.lucena27@gmail.com>
2024-01-16 19:52:37 -0300

    removendo credenciais do banco

--- assets/php/connect.php
+++ assets/php/connect.php
@@ -1,12 +1,6 @@
 <?php
-// Configurações do banco de dados
-// $host = 'aws.connect.psdb.cloud';
-// $usuario = 'o6hpbqzg0eoxsn3nwrar'; //REMOVER AS CREDENCIAIS ANTES DE DAR PUSH NO REPOSITÓRIO.
-// $senha = 'pscale_pw_ER9ns7YXMm93xQ0QMKJqITB913bwdvk1JCUFj7R0rE1'; //REMOVER AS CREDENCIAIS ANTES DE DAR PUSH NO REPOSITÓRIO.
-$banco = 'exemplifiqui-db'; //REMOVER AS CREDENCIAIS ANTES DE DAR PUSH NO REPOSITÓRIO.
 
-
-
+$banco = getenv('DB_NAME');
 $host = getenv('DB_HOST');
 $usuario = getenv('DB_USER');
 $senha = getenv('DB_PASSWORD');
