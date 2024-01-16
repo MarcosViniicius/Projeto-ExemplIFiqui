ac4dc47939471a173a8311932fad4e6aec77a962
Marcos Vinicius de lucena <marcosvinicius.lucena27@gmail.com>
2024-01-16 19:19:37 -0300

    Modificando as conexões com o banco de dados

--- assets/php/connect.php
+++ assets/php/connect.php
@@ -1,11 +1,14 @@
 <?php
 // Configurações do banco de dados
-// $host = 'newsletter-exemplifiqui.c1iccz3gz8vt.us-east-1.rds.amazonaws.com:3306';
+// $host = 'aws.connect.psdb.cloud';
+// $usuario = 'o6hpbqzg0eoxsn3nwrar'; //REMOVER AS CREDENCIAIS ANTES DE DAR PUSH NO REPOSITÓRIO.
+// $senha = 'pscale_pw_ER9ns7YXMm93xQ0QMKJqITB913bwdvk1JCUFj7R0rE1'; //REMOVER AS CREDENCIAIS ANTES DE DAR PUSH NO REPOSITÓRIO.
+$banco = 'exemplifiqui-db'; //REMOVER AS CREDENCIAIS ANTES DE DAR PUSH NO REPOSITÓRIO.
+
+
+
 $host = getenv('DB_HOST');
-// $usuario = 'admin'; //REMOVER AS CREDENCIAIS ANTES DE DAR PUSH NO REPOSITÓRIO.
 $usuario = getenv('DB_USER');
-// $senha = 'Mini1000'; //REMOVER AS CREDENCIAIS ANTES DE DAR PUSH NO REPOSITÓRIO.
 $senha = getenv('DB_PASSWORD');
-$banco = 'exemplifiqui'; //REMOVER AS CREDENCIAIS ANTES DE DAR PUSH NO REPOSITÓRIO.
 // SUBSTITUIR AS VARIÁVEIS DE AMBIENTE PELAS CREDENCIAIS DO BANCO PARA EXECUTAR LOCALMENTE
 ?>
--- assets/php/login.php
+++ assets/php/login.php
@@ -1,11 +1,25 @@
 <?php
 require_once('connect.php');
-// Conexão com o banco de dados
-$conexao = mysqli_connect($host, $usuario, $senha, $banco);
 
-// Verifica se houve erro na conexão
-if (mysqli_connect_errno()) {
-    echo "Falha ao conectar ao banco de dados: " . mysqli_connect_error();
+// Configurações de conexão com o PlanetScale
+$hostURL = $host;
+$databaseName = $banco;
+$user = $usuario;
+$dbP = $senha;
+
+
+$dsn = 'mysql:host=' . $hostURL . ';dbname=' . $databaseName . ';port=3306';
+$options = array(
+    PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/cert.pem',
+    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+);
+
+// Conexão com o banco de dados usando PDO
+try {
+    $db = new PDO($dsn, $user, $dbP, $options);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $error) {
+    echo "Falha ao conectar ao banco de dados: " . $error->getMessage();
 }
 
 // Processar o formulário quando for enviado
@@ -15,10 +29,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = $_POST["password"];
 
     // Consulta SQL para verificar se o usuário existe
-    $sql = "SELECT * FROM usuario_comum WHERE nome_de_usuario = '$username' AND senha_criptografada = '$password'";
-    $resultado = mysqli_query($conexao, $sql);
+    $sql = "SELECT * FROM usuario_comum WHERE nome_de_usuario = :username AND senha_criptografada = :password";
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+    $stmt->bindParam(':password', $password, PDO::PARAM_STR);
+    $stmt->execute();
+
     // Verifica se a consulta retornou algum resultado
-    if (mysqli_num_rows($resultado) == 1) {
+    if ($stmt->rowCount() == 1) {
         // Usuário autenticado com sucesso
         echo "Login realizado com sucesso!";
         // Redireciona para a tela de painel de controle
@@ -30,8 +48,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "<script src='../assets/js/error.js'></script>";
         $error_message = "Nome de usuário ou senha incorretos.";
     }
-
-    // Fecha a conexão com o banco de dados
-    mysqli_close($conexao);
 }
+
+// Não é mais necessário fechar a conexão explicitamente com PDO, pois é fechada automaticamente quando o script termina.
 ?>
